6894e8c33b55034d0bf20f39ec6f504e650688c5
Taiwo Mercy <kennyt341@gmail.com>
2023-02-25 17:33:24 +0100

    Completed hero of landing page

---
+++ component/leading/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Leading = () => {
+  return <div></div>
+}
+
+export default Leading
--- component/lefthero/index.tsx
+++ component/lefthero/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { homePageType } from '@/types/home'
+import styles from './Lefthero.module.css'
+import Image from 'next/image'
 
 const Lefthero = (props: homePageType) => {
-  return <div></div>
+  return (
+    <div className={styles.container}>
+      <Image
+        width={400}
+        height={400}
+        src='/pics/Hero.png'
+        alt='hero Image'
+        className={styles.image}
+      />
+    </div>
+  )
 }
 
 export default Lefthero
