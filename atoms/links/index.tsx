3be3618cc58f7a1f883c89161f049cc240e4c1f5
Taiwo Mercy <kennyt341@gmail.com>
2023-03-03 06:34:01 +0100

    Made footer items links

--- atoms/links/index.tsx
+++ atoms/links/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { datt } from '@/data/datts'
 import styles from './Links.module.css'
+import Link from 'next/link'
 
-const Links = (props: any) => {
+const Links = () => {
   return (
     <div className={styles.container}>
       {datt.map((item: any) => (
@@ -10,7 +11,7 @@ const Links = (props: any) => {
           <h4 className={styles.head}>{item.head}</h4>
           {item.bod.map((item: any, index: any) => (
             <p className={styles.text} key={index}>
-              {item}
+              <Link href={`/${item}`}>{item}</Link>
             </p>
           ))}
         </div>
